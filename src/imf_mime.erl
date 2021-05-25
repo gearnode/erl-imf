faa2bb998df87ecde086b421d407434660b27add
gearnode <bryan@frimin.fr>
2021-05-25 13:17:41 +0200

    Add draft mime part support

--- src/imf_mime.erl
+++ src/imf_mime.erl
@@ -14,8 +14,67 @@
 
 -module(imf_mime).
 
--export([encode_version/1]).
+-export([encode_version/1, encode_part/1]).
+
+-export_type([part/0, header/0, field/0, body/0]).
+
+-type part() :: #{header := header(),
+                  body := body()}.
+
+-type header() :: [field()].
+
+-type field() :: content_type()
+               | content_transfer_encoding()
+               | content_id()
+               | content_description().
+
+-type content_type() :: {content_type, media_type()}.
+
+-type content_transfer_encoding() :: {content_transfer_encoding, mechanism()}.
+
+-type content_id() :: {content_id, imf:msg_id()}.
+
+-type content_description() :: {content_description, text()}.
+
+-type mechanism() :: '7bit' | '8bit' | binary | quoted_printable | base64.
+
+-type media_type() ::
+        #{type := binary(), subtype := binary(),
+          parameters => #{attribute() => value()}}.
+
+-type attribute() :: binary().
+-type value() :: binary().
+
+-type text() :: binary().
+
+-type body() :: {data, iodata()}
+              | {part, part()}
+              | {part, [part()]}.
 
 -spec encode_version(imf:mime_version()) -> iodata().
 encode_version({Major, Minor}) ->
   io_lib:format("~B.~B", [Major, Minor]).
+
+-spec encode_part(part()) -> iodata().
+encode_part(#{header := Fields}) ->
+  lists:reverse(lists:foldl(fun encode_field/2, [], Fields)).
+
+-spec encode_field(field(), iodata()) -> iodata().
+encode_field({content_transfer_encoding, Mechanism}, Acc) ->
+  [["Content-Transfer-Encoding: ", encode_mechanism(Mechanism)] | Acc];
+encode_field({content_id, Id}, Acc) ->
+  [["Content-ID: ", imf_message_id_field:encode(Id)] | Acc];
+encode_field({content_description, Bin}, Acc) ->
+  [["Content-Description:", imf_unstructured_field:encode(Bin, 20)] | Acc].
+
+-spec encode_mechanism(mechanism()) -> iodata().
+encode_mechanism('7bit') ->
+  "7bit";
+encode_mechanism('8bit') ->
+  "8bit";
+encode_mechanism(binary) ->
+  "binary";
+encode_mechanism(quoted_printable) ->
+  "quoted-printable";
+encode_mechanism(base64) ->
+  "base64".
